aedc82f04f97008695337f375c00ffc97f7d1376
briney <bryanbriney@gmail.com>
2018-07-20 10:18:10 -0700

    initial commit for tests

---
+++ abutils/tests/test_alignment.py
@@ -0,0 +1,35 @@
+#!usr/env/python
+# filename: test_alignment.py
+
+from ..core.sequence import Sequence
+from ..utils import alignment
+
+
+QUERY = Sequence('ATGCATGC', id='query')
+ALN_QUERY = 'ATGCATGC'
+TARGET = Sequence('ATGATGC', id='target')
+ALN_TARGET = 'ATG-ATGC'
+
+
+def test_muscle():
+    aln = alignment.muscle(sequences=[QUERY, TARGET])
+    aln_query = [a for a in aln if a.id == 'query'][0]
+    aln_target = [a for a in aln if a.id == 'target'][0]
+    assert str(aln_query.seq) == ALN_QUERY and str(aln_target.seq) == ALN_TARGET
+
+
+def test_mafft():
+    aln = alignment.mafft(sequences=[QUERY, TARGET])
+    aln_query = [a for a in aln if a.id == 'query'][0]
+    aln_target = [a for a in aln if a.id == 'target'][0]
+    assert str(aln_query.seq).upper() == '---ATGCATGC' and str(aln_target.seq).upper() == 'ATGATGC----'
+
+
+def test_local_alignment():
+    aln = alignment.local_alignment(QUERY, TARGET, gap_open=-1)
+    assert aln.aligned_query == ALN_QUERY and aln.aligned_target == ALN_TARGET
+
+
+def test_global_alignment():
+    aln = alignment.global_alignment(QUERY, TARGET, gap_open=-1)
+    assert aln.aligned_query == ALN_QUERY and aln.aligned_target == ALN_TARGET
---
+++ abutils/tests/test_sequence.py
@@ -0,0 +1,17 @@
+#!usr/env/python
+# filename: test_alignment.py
+
+import os
+from ..core.sequence import Sequence, read_fasta, read_json
+
+
+def test_read_fasta():
+    seqs = read_fasta(os.path.abspath('tests/data/PG9.fasta'))
+    assert len(seqs) == 1 and seqs[0].id == 'PG9'
+
+
+def test_read_json():
+    seqs = read_json(os.path.abspath('tests/data/PG9.json'))
+    assert len(seqs) == 1 and seqs[0].id == 'PG9'
+
+
