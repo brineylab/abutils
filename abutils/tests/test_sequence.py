e1192669127a449d46ad0d0b3a34e80db77ac0cd
briney <bryanbriney@gmail.com>
2018-07-20 16:52:32 -0700

    fix test data path

--- abutils/tests/test_sequence.py
+++ abutils/tests/test_sequence.py
@@ -6,12 +6,12 @@ from ..core.sequence import Sequence, read_fasta, read_json
 
 
 def test_read_fasta():
-    seqs = read_fasta('data/PG9.fasta')
+    seqs = read_fasta(os.path.abspath('abutils/tests/data/PG9.fasta'))
     assert len(seqs) == 1 and seqs[0].id == 'PG9'
 
 
 def test_read_json():
-    seqs = read_json(os.path.abspath('tests/data/PG9.json'))
+    seqs = read_json(os.path.abspath('abutils/tests/data/PG9.json'))
     assert len(seqs) == 1 and seqs[0].id == 'PG9'
 
 
