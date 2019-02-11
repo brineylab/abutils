14dd0110ba1d2902ca2b50fc15492ff8cf319652
briney <bryanbriney@gmail.com>
2019-02-10 18:01:31 -0800

    Update __init__.py

--- abutils/__init__.py
+++ abutils/__init__.py
@@ -3,6 +3,8 @@
 
 import os
 
+from .utils import alignment, cluster, decorators, jobs, log, phylogeny, pipeline, progbar, s3, utilities
+
 from .version import __version__
 
 BINARY_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'bin'))
