f2f929fc70c1b6a0e4d3d29c1cc882ede9950181
briney <bryanbriney@gmail.com>
2019-03-03 15:00:35 -0800

    properly handle mongodb sequences missing the seq_field (as_list())

--- abutils/utils/seqio.py
+++ abutils/utils/seqio.py
@@ -262,6 +262,8 @@ class MongoDBInput(BaseInput):
                 print(collection)
             res = self.db[collection].find(self.query, self.projection)
             for r in res:
+                if self.seq_field not in r:
+                    continue
                 sequences.append(Sequence(r, seq_key=self.seq_field))
         return sequences
 
