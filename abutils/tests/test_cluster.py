a5633cd16867e6388045249fd5c048d4045a46b2
briney <bryanbriney@gmail.com>
2019-06-14 13:22:47 -0700

    update cluster tests for mmseqs

--- abutils/tests/test_cluster.py
+++ abutils/tests/test_cluster.py
@@ -15,32 +15,32 @@ CENTROID = 'ATGCATGCATGCATGCATGCATGC'
 TEMP = os.path.abspath('./')
 
 
-def test_cdhit():
-    ofile, cfile = cluster.cdhit(CLUSTER_SEQS, make_db=False, debug=True)
-    errors = []
-    if not os.path.getsize(ofile):
-        errors.append('CDHIT output file ({}) is empty'.format(ofile))
-    if not os.path.getsize(cfile):
-        errors.append('CDHIT cluster file ({}) is empty'.format(cfile))
-    os.unlink(ofile)
-    os.unlink(cfile)
-    assert len(errors) == 0, '\n'.join(errors)
+# def test_cdhit():
+#     ofile, cfile = cluster.cdhit(CLUSTER_SEQS, make_db=False, debug=True)
+#     errors = []
+#     if not os.path.getsize(ofile):
+#         errors.append('CDHIT output file ({}) is empty'.format(ofile))
+#     if not os.path.getsize(cfile):
+#         errors.append('CDHIT cluster file ({}) is empty'.format(cfile))
+#     os.unlink(ofile)
+#     os.unlink(cfile)
+#     assert len(errors) == 0, '\n'.join(errors)
 
 
-def test_parse_clusters():
-    seq_dict = {s.id: s for s in CLUSTER_SEQS}
-    ofile, cfile = cluster.cdhit(CLUSTER_SEQS, make_db=False, threshold=0.9, debug=True)
-    cdhit_result = cluster.parse_clusters(ofile, cfile, seq_dict=seq_dict)
-    assert len(cdhit_result) == 1
+# def test_parse_clusters():
+#     seq_dict = {s.id: s for s in CLUSTER_SEQS}
+#     ofile, cfile = cluster.cdhit(CLUSTER_SEQS, make_db=False, threshold=0.9, debug=True)
+#     cdhit_result = cluster.parse_clusters(ofile, cfile, seq_dict=seq_dict)
+#     assert len(cdhit_result) == 1
 
 
 def test_cluster_centroid():
-    cdhit_result = cluster.cluster(CLUSTER_SEQS, make_db=False, threshold=0.9, debug=True)
-    c = cdhit_result.clusters[0]
+    clu = cluster.cluster(CLUSTER_SEQS, threshold=0.9, debug=True)
+    c = clu[0]
     assert c.centroid.sequence == CENTROID
 
 
 def test_cluster_consensus():
-    cdhit_result = cluster.cluster(CLUSTER_SEQS, make_db=False, threshold=0.9, debug=True)
-    c = cdhit_result.clusters[0]
+    clu = cluster.cluster(CLUSTER_SEQS, threshold=0.9, debug=True)
+    c = clu[0]
     assert c.consensus.sequence == CONSENSUS
