22a63367697463921717a858936b846703e94fca
briney <bryanbriney@gmail.com>
2019-06-17 15:35:30 -0700

    add PyPI versions

--- setup.py
+++ setup.py
@@ -44,7 +44,12 @@ config = {
     'packages': ['abutils'],
     'scripts': [],
     'name': 'abutils',
-    'include_package_data': True
+    'include_package_data': True,
+    'classifiers': ['License :: OSI Approved :: MIT License',
+                    'Programming Language :: Python :: 3.5',
+                    'Programming Language :: Python :: 3.6',
+                    'Programming Language :: Python :: 3.7',
+                    'Topic :: Scientific/Engineering :: Bio-Informatics']
 }
 
 setup(**config)
