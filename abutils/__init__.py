1fd51d87784103d47ffd3a6860b30971a2e5bc40
Bryan Briney <bryanbriney@gmail.com>
2023-02-10 00:44:54 -0800

    add SSWAlignment and NWalignment top level __init__

--- abutils/__init__.py
+++ abutils/__init__.py
@@ -12,10 +12,23 @@ from .core.sequence import Sequence
 from .core.pair import Pair
 from .core.lineage import Lineage
 
-from .utils import alignment, cluster, decorators, jobs, log, phylogeny, pipeline, progbar, s3, seqio, utilities
+from .utils import (
+    alignment,
+    cluster,
+    decorators,
+    jobs,
+    log,
+    phylogeny,
+    pipeline,
+    progbar,
+    s3,
+    seqio,
+    utilities,
+)
 from .utils import alignment as aln
 from .utils import pipeline as path
+from utils.alignment import SSWAlignment, NWAlignment
 
 from .version import __version__
 
-BINARY_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'bin'))
+BINARY_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "bin"))
