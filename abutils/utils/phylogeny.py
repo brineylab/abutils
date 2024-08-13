db5118322f1c4a8218b5f3a09ab6226553cd9e0b
Bryan Briney <bryanbriney@gmail.com>
2024-08-13 12:59:06 -0700

    remove unnecessary imports

--- abutils/utils/phylogeny.py
+++ abutils/utils/phylogeny.py
@@ -41,8 +41,8 @@ from collections import Counter
 from copy import copy, deepcopy
 from typing import Optional
 
-import abstar
-import ete3
+# import abstar
+# import ete3
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
